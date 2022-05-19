089d40146bb3774847e19fbe344cd79bd6af229c
Momotofff <momotofff@mail.ru>
2022-05-20 01:30:58 +0400

    start new project

--- app/src/main/java/com/example/it/MainActivity.java
+++ app/src/main/java/com/example/it/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startIT(View v)
+    {
+        Intent intent = new Intent(this,MainActivityQA.class);
+        startActivity(intent);
     }
 }
