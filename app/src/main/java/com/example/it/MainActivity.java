f385b8df99786f271a23104f573db2e43c48516c
Momotofff <momotofff@mail.ru>
2022-05-26 23:57:14 +0400

    My the first fucking animation)))

--- app/src/main/java/com/example/it/MainActivity.java
+++ app/src/main/java/com/example/it/MainActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.start_image_view);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.start_anim);
+        iv.startAnimation(animation);
     }
 
-    public void startIT(View v)
+    public void startApp(View view)
     {
-        Intent intent = new Intent(this,MainActivityThemes.class);
+
+        Intent intent = new Intent(this, MainActivityThemes.class);
         startActivity(intent);
+       // finish();
     }
+
 }
--- app/src/main/java/com/example/it/MainActivityThemes.java
+++ app/src/main/java/com/example/it/MainActivityThemes.java
@@ -1,27 +1,32 @@
 package com.example.it;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivityThemes extends AppCompatActivity
 {
+    int[] pushRandom = {R.string.push_random0, R.string.push_random1, R.string.push_random2, R.string.push_random3,
+                        R.string.push_random4, R.string.push_random5, R.string.push_random6, R.string.push_random7,
+                        R.string.push_random8, R.string.push_random9, R.string.push_random10, R.string.push_random11,
+                        R.string.push_random12, R.string.push_random13, R.string.push_random14,R.string.push_random15,
+                        R.string.push_random16, R.string.push_random17, R.string.push_random18,R.string.push_random19,
+                        R.string.push_random20, R.string.push_random21, R.string.push_random22};
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_themes);
+        TextView textView = findViewById(R.id.push_random);
+        textView.setText(pushRandom[(int) (Math.random() * pushRandom.length)]);
     }
 
-    public void startQA(View v)
+    public void buttonStartInDeveloping(View view)
     {
-        Intent intent = new Intent(this,MainActivityQAInside.class);
-        startActivity(intent);
-    }
-    public void startJava(View v)
-    {
-        Intent intent = new Intent(this,MainActivityQAInside.class);
-        startActivity(intent);
+            Toast toast = Toast.makeText(MainActivityThemes.this, "В разработке", Toast.LENGTH_SHORT);
+            toast.show();
     }
 }
