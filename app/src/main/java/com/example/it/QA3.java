f5c180c3a97c694eb057d80dd6804256529a279d
Momotofff <momotofff@mail.ru>
2022-06-27 23:25:39 +0400

    new View) all thematic activity finish.

--- app/src/main/java/com/example/it/QA3.java
+++ app/src/main/java/com/example/it/QA3.java
@@ -1,15 +1,14 @@
 package com.example.it;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class QA1 extends AppCompatActivity {
+public class QA3 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qa1);
-
+        setContentView(R.layout.activity_qa3);
     }
 }
